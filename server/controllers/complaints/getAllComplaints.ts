1b97b8f13c3d67b9b11a31c35be16f0a9eb0906b
abeerghazal <abeerghazal94@gmail.com>
2024-10-14 18:06:35 +0300

    Build Application and push it to Github

---
+++ server/controllers/complaints/getAllComplaints.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { ComplaintsModel, UserModel, FlatModel } from '../../models';
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await ComplaintsModel.findAll({
+      raw: true,
+      include: [
+        {
+          model: UserModel,
+          attributes: [], // Exclude User attributes
+          include: [
+            {
+              model: FlatModel,
+              attributes: ['flat_number'], // Only include flat_number
+            },
+          ],
+        },
+      ],
+    });
+
+    const result = data.map((complaint) => ({
+      id: complaint.id,
+      title: complaint.title,
+      description: complaint.description,
+      flat_number: complaint['User.Flats.flat_number'],
+    }));
+
+    res.json({ result });
+  } catch (error) {
+    next(error);
+  }
+};
